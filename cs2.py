7a03f6949535c449cdccfbcc00f25c8ec63e4b18
Monica Bustamante <lilianalambda@gmail.com>
2020-09-22 12:42:50 -0700

    practice and analysis

---
+++ cs2.py
@@ -0,0 +1,96 @@
+# 232. Implement Queue using Stacks
+# Easy
+# Add to List
+
+# Share
+# Implement the following operations of a queue using stacks.
+
+# push(x) -- Push element x to the back of queue.
+# pop() -- Removes the element from in front of queue.
+# peek() -- Get the front element.
+# empty() -- Return whether the queue is empty.
+# Example:
+
+# MyQueue queue = new MyQueue();
+
+# queue.push(1);
+# queue.push(2);  
+# queue.peek();  // returns 1
+# queue.pop();   // returns 1
+# queue.empty(); // returns false
+# Notes:
+
+# You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
+# Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
+# You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
+
+class MyQueue(object):
+    
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.s1=[]
+        self.s2=[]
+
+    def push(self, x):
+        """
+        Push element x to the back of queue.
+        :type x: int
+        :rtype: None
+        """
+        while self.s1:
+            self.s2.append(self.s1.pop())
+        self.s1.append(x)
+        while self.s2:
+            self.s1.append(self.s2.pop())
+
+    def pop(self):
+        """
+        Removes the element from in front of queue and returns that element.
+        :rtype: int
+        """
+        return self.s1.pop()
+
+    def peek(self):
+        """
+        Get the front element.
+        :rtype: int
+        """
+        return self.s1[-1]
+
+    def empty(self):
+        """
+        Returns whether the queue is empty.
+        :rtype: bool
+        """
+        return not self.s1
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
+
+class MyQueue:
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x):
+        self.s1.append(x)
+
+    def pop(self):
+        self.peek()
+        return self.s2.pop()
+
+    def peek(self):
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2[-1]        
+
+    def empty(self):
+        return not self.s1 and not self.s2
--- practice.py
+++ practice.py
@@ -140,3 +140,72 @@ def candies(n, m):
 
 def seatsInTheater(nCols, nRows, col, row):
     return (nCols-col+1)*(nRows-row)
+
+# Given a divisor and a bound, find the largest integer N such that:
+# N is divisible by divisor.
+# N is less than or equal to bound.
+# N is greater than 0.
+# It is guaranteed that such a number exists.
+# Example
+# For divisor = 3 and bound = 10, the output should be
+# maxMultiple(divisor, bound) = 9.
+
+# The largest integer divisible by 3 and not larger than 10 is 9.
+def maxMultiple(divisor, bound):
+    for num in range(bound, 1, -1):
+        if num % divisor == 0:
+            return num
+    return 0 
+
+
+
+# Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the distance between any two neighboring numbers is equal (note that 0 and n - 1 are neighboring, too).
+# Given n and firstNumber, find the number which is written in the radially opposite position to firstNumber.
+# Example
+# For n = 10 and firstNumber = 2, the output should be
+# circleOfNumbers(n, firstNumber) = 7.
+
+def circleOfNumbers(n, firstNumber):
+    return (firstNumber + (n//2))%n
+
+# One night you go for a ride on your motorcycle. At 00:00 you start your engine, and the built-in timer automatically begins counting the length of your ride, in minutes. Off you go to explore the neighborhood.
+# When you finally decide to head back, you realize there's a chance the bridges on your route home are up, leaving you stranded! Unfortunately, you don't have your watch on you and don't know what time it is. All you know thanks to the bike's timer is that n minutes have passed since 00:00.
+# Using the bike's timer, calculate the current time. Return an answer as the sum of digits that the digital timer in the format hh:mm would show.
+# Example
+# For n = 240, the output should be
+# lateRide(n) = 4.
+# Since 240 minutes have passed, the current time is 04:00. The digits sum up to 0 + 4 + 0 + 0 = 4, which is the answer.
+# For n = 808, the output should be
+# lateRide(n) = 14.
+# 808 minutes mean that it's 13:28 now, so the answer should be 1 + 3 + 2 + 8 = 14.
+
+def lateRide(n):
+    hours = n // 60
+    minutes = n % 60
+    return (hours // 10) + (hours % 10) + (minutes // 10) + (minutes % 10)
+
+# Some phone usage rate may be described as follows:
+# first minute of a call costs min1 cents,
+# each minute from the 2nd up to 10th (inclusive) costs min2_10 cents
+# each minute after 10th costs min11 cents.
+# You have s cents on your account before the call. What is the duration of the longest call (in minutes rounded down to the nearest integer) you can have?
+# Example
+# For min1 = 3, min2_10 = 1, min11 = 2, and s = 20, the output should be
+# phoneCall(min1, min2_10, min11, s) = 14.
+# Here's why:
+# the first minute costs 3 cents, which leaves you with 20 - 3 = 17 cents;
+# the total cost of minutes 2 through 10 is 1 * 9 = 9, so you can talk 9 more minutes and still have 17 - 9 = 8 cents;
+# each next minute costs 2 cents, which means that you can talk 8 / 2 = 4 more minutes.
+# Thus, the longest call you can make is 1 + 9 + 4 = 14 minutes long.
+
+def phoneCall(min1, min2_10, min11, s):
+    if s<min1:
+        return 0
+    if s == min1:
+        return 1
+    if s <= min1 + (min2_10*9):
+        s -= min1
+        return (s//min2_10) +1
+    s -= min2_10 * 9
+    s -= min1
+    return (s//min11) + 10
